4cb227532f480351ae5f3b23451613f700582561
Paolo <71812497+paolomartini25@users.noreply.github.com>
2020-10-15 12:01:10 +0200

    Add files via upload

--- Run_Length_Encoding.c
+++ Run_Length_Encoding.c
@@ -2,64 +2,98 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX 1000
 
+void Encoding(char* var);  //--> funzione per l'Encoding
+void Decoding(char* var);  //--> funzione per il Decoding
 
-void decoding();
-void endecoding();
+int main () {
+    short scelta;  //variabile per la scelta tra Encoding e Decoding
+    char var[MAX];   //stringa
 
-int main(){
+    printf ("Cosa vuoi fare? \nEncoding\t0\n\t\tDecoding\t1\n");
+    scanf ("%d", &scelta);
 
-    int coding = 0;
-    char carattere = 0;
-    int num=0;
-    char var[100];
-    char dec[100];
-
-    printf("Vuoi fare decoding o endecoding?\nPremi 0 per fare decoding\tPremi 1 perfare endecoding\n");
-    scanf("%d", coding);
-
-    switch (coding)
+    //switch che sceglie tra le due funzioni
+    switch (scelta)
     {
     case 0:
-//-----------------------------------------------------------------------------------------------
-        
-        printf("Inserisci la stringa senza spazi.");
-        gets(var);
-
-//-----------------------------------------------------------------------------------------------
+        Encoding(var);
         break;
-    
-    default:
-//-----------------------------------------------------------------------------------------------
-    
-    
-        printf("Inserisci la stringa con sole lettere, senza spazi.");
-        gets(var);
-    int a = -1;
-    char b;
-    do
-    {
-        a++;
-        b=var[a];
-    } while (b!="/0");
-    
-
-    for(int i = 0; i>a; i++){
-        if (carattere == var[i]){
-            for(int j = 0; i>a && carattere!=var[i]; j++){
-                num++;
-                carattere = var[i];
-            }
-            dec[i]=num;
-        }
-        else{
-            dec[i]=var[i];
-        }
-        var[i]=carattere;
-    }
-    //----------------------------------------------------------------------------------------
+    case 1:
+        Decoding(var);
         break;
+    default: 
+        printf ("Errore: reiserisci la scelta");
+        printf ("Cosa vuoi fare? \nEncoding\t0\n\t\tDecoding\t1\n");
+        scanf ("%d", &scelta);
     }
-    printf("%s", dec);
+
+    return 0;
 }
 
+void Encoding(char* var) {
+    int length;         //lunghezza della stringa
+    int cont = 1;       //conta quante lettere sono uguali, parte da 1 perchè il minimo di lettere è 1
+    int a, b;           //inizializzo le variabili che utilizzerò nel for
+
+    printf ("enter the string: ");
+    scanf ("%s", var);
+
+    //misura la lungezza della stringa
+    for (a = 0; var[a] != '\0'; a++) {
+        length = a;
+    }
+
+    //guardo ogni cella del vettore
+    for (b = 0; b <= length; b++) {
+
+        if (var[b] == var[b+1]) { //se la lettera della posizione precedente è uguale alla lettera nella posizione successiva:
+            cont++;     //incremento il contatore delle lettere uguali 
+        }
+        else { //se la lettera della posizione precedente è diversa alla lettera nella posizione successiva:
+            printf("%d%c", cont, var[b]);//stampo il numero di lettere uguaali e la lettara
+            cont = 1;
+        }
+    }
+    printf ("Fine encoding.\n");
+}
+
+void Decoding(char* var) {
+    int length;                         // lunghezza della stringa
+    int cont = 0;                       // contatore delle lettere, parte da 0 perchè non si sa se sono presenti tutte le lettere
+    int a, b, c, d;                  // inizializzo levaraibili per i for
+
+    printf ("enter the string: ");
+    scanf ("%s", var);
+
+    //misura la lungezza della stringa
+    for (a = 0; var[a] != '\0'; a++) {
+        length = a;
+    }
+
+    printf ("\n\tOperazione di Decoding in corso...\n\n");
+
+    //controllo il numero di volte in cui bisogna stampare il carattere
+    for (b = 0; b <= length; b++) {
+
+        for (c = 0; c < 9; c++) {
+
+            //individua il carattere numero
+            if (var[b] <= '1' && var[b] >= '9') {
+                cont = var[b] - '0';      //salvo il carattere numero in cont
+            }
+            else {
+
+                //stampo la stringa
+                for (d = 0; d < cont; d++) {
+                    printf("%c", var[b]);
+                }
+
+                cont = 0;
+            }
+        }
+    }
+
+    printf ("\n\n\tProgramma terminato...\n");
+}
