fe37a91390553e5334f916919db20eddf8a11b79
Paolo <71812497+paolomartini25@users.noreply.github.com>
2020-11-12 12:45:36 +0100

    Add files via upload

--- es3/es3.c
+++ es3/es3.c
@@ -5,7 +5,6 @@
 #include<stdbool.h>
 
 #define DIM 50
-#define NCANZONI 11
 #define CARATTERE ","
 
 
@@ -23,7 +22,7 @@ int contarighe(FILE *fp);
 
 void lettura(FILE *fp, Tiporecord *canzoni);
 
-void stampa_r(FILE *fp, Tiporecord *canzoni);
+void stampa_r(FILE *fp, Tiporecord *canzoni, int ncanzoni); //richiamo il file, l'array di struct e il numero di canzoni
 
 int main(){
     FILE* puntatore=fopen("sputifi.csv", "r");
@@ -40,35 +39,31 @@ int main(){
 
     lettura(puntatore, carica);
 
-    stampa_r(puntatore, carica);
+    stampa_r(puntatore, carica, n_righe); 
+    free(carica);//
 
     fclose (puntatore);
     return 0;
 }
 
 void lettura(FILE *fp, Tiporecord *canzoni){
+//la funzione legge il file e carica nell'array di strutture le varie componenti
     int cont=0;
     char stringa[DIM]; 
     char *field;//è un arrey di char in cui non è definita la lunghezza
     
     while(fgets(stringa,DIM,fp)){
-        
-        /*
-		fgets(stringa,DIM,fp);legge le righe del file una riga dopo l'altra (nome della variabile in vui salva la riga che hai letto, dimensione, file)
-        fgets da falso quando finisce il file
-        printf("%s\n",stringa);
-        */
            
         field = strtok(stringa, CARATTERE); //salva in field la prima parte separata da stringa; strtok si tiene in memoria il numero della riga e quindi continua a lavorare sulla stessa riga
-        canzoni[cont].numero = atoi(field); //atoi da stringa a numero intero; 
+        (canzoni+cont)->numero = atoi(field); //atoi da stringa a numero intero; 
         //numero,nome,autore
 
         field = strtok(NULL, CARATTERE);
-        strcpy(canzoni[cont].nome, field);//prende in memoria una stringa e la duplica in un altra variabile
+        strcpy((canzoni+cont)->nome, field);//prende in memoria una stringa e la duplica in un altra variabile
         //nome,autore
 
         field = strtok(NULL, CARATTERE);    
-        strcpy(canzoni[cont].autore, field);
+        strcpy((canzoni+cont)->autore, field);
         //autore
         
         cont++;  
@@ -76,39 +71,40 @@ void lettura(FILE *fp, Tiporecord *canzoni){
     }
 }
 
-void stampa_r(FILE *fp, Tiporecord *canzoni){
-
+void stampa_r(FILE *fp, Tiporecord *canzoni, int ncanzoni){
+//stampa in modo randomico le canzoni
     int nriproduzioni;
     int rm;
     printf("Quante volte vuoi riprodurre la playlist? ");
     scanf("%d", &nriproduzioni);
-    for (int n = 0; n < NCANZONI; n++){
-        canzoni[n].riprodotte = false;                               //se è false la canzone non è stata riprodotta mentre se è true è già stata riprodotta
+    for (int n = 0; n < ncanzoni; n++){
+        (canzoni+n)->riprodotte = false;                               //se è false la canzone non è stata riprodotta mentre se è true è già stata riprodotta
     }
     for (int i = 0; i < nriproduzioni; i++) {                              //si riproducono tutte le canzoni nriproduzioni volte
         printf("Sto riproducendo per la %d^ volta.\n\n", i+1);
 
-        for (int j = 0; j < NCANZONI; j++)                                //per ogni canzone                                    
+        for (int j = 0; j < ncanzoni; j++)                                //per ogni canzone                                    
             {
                 do{
-                    rm = rand() % NCANZONI;
-                }while (canzoni[rm].riprodotte);                         //ripete finchè la canzone non è stata riprodotta: quindi riprodotte==false
+                    rm = rand() % ncanzoni;
+                }while ((canzoni+rm)->riprodotte);                         //ripete finchè la canzone non è stata riprodotta: quindi riprodotte==false
                     
-                canzoni[rm].riprodotte = true;                            //esce dal do while solo se tutte la canzoni sono state riprodotte
-                printf("sto riproducendo:\n%s\n%s\t\n\n", canzoni[rm].nome, canzoni[rm].autore);         
+                 (canzoni+rm)->riprodotte = true;                            //esce dal do while solo se tutte la canzoni sono state riprodotte
+                printf("sto riproducendo:\n%s\n%s\t\n\n", (canzoni+rm)->nome, (canzoni+rm)->autore);         
             }
-            for (int k = 0; k < NCANZONI; k++){
-                canzoni[k].riprodotte = false;                                   //resetto la playlist
+            for (int k = 0; k < ncanzoni; k++){
+                (canzoni+k)->riprodotte = false;                                   //resetto la playlist
             }
     }
 
 }
 
 int contarighe(FILE *fp){
-    int cont = 0;
+//conta le righe del file
+    int cont = 0; //contatore delle righe
     char stringa[DIM];
-    while (fgets(stringa,DIM,fp)){
-        cont++;
+    while (fgets(stringa,DIM,fp)){ //scorro le varie righe del file
+        cont++; //aggiungo una riga
     }
     return cont;
 }
