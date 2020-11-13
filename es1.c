4843e7d4912955955f090aeaa27021836a39111c
Paolo <71812497+paolomartini25@users.noreply.github.com>
2020-11-13 11:40:34 +0100

    Add files via upload

--- es1.c
+++ es1.c
@@ -1,52 +1,53 @@
 //Paolo Martini 4^B ROB
+//calcola i giorni in cui vengono contagiati un certo numero di persone
 
 #include <stdio.h>
 #include <stdlib.h>
 
-float R, N;
-int giorni;
+void richiesta_dati(float *R, int *N);
 
-void questions();
-int calculations();
+int calcolo_contagiati(int *contagiati, int N, int R);
 
 int main()
 {
+	float R = 0; //percentuale contagio
+	int N = 0; //numero di alunni
 
-    questions();
-    int tg = calculations();
-    printf("\nCi vogliono %d giorni per contagiare tutti! \n",tg);
-
-    return 0;
+	int* contagiati = (int*)malloc(sizeof(int)); //vettore che contiene il numero di contagiati
+	
+	richiesta_dati(&R, &N);
+	
+    int n_giorni = calcolo_contagiati(contagiati, N, R);
+	
+	printf("Giorno %d:\nSono stati contagiati tutti!\n\n", n_giorni);
+    
+	return 0;
 }
 
-void questions(){
+void richiesta_dati(float *R, int *N){
+    //richiesta dei dati in input
+    printf("inserire il valore di contagio\n");
+	scanf("%f", R); //richiedo la percentuale di contagio
 
-    printf("Quante persone contagia una persona in media? (suggerimento 1.4)\n");
-    scanf("%f", &R);
-    printf("Quanti compagni di classe hai? (esempio 21)\n");
-    scanf("%f", &N);
-    printf("Qual'e' il limite di giorni massimo? \n");
-    scanf("%d", &giorni);
-
+	printf("Quanti alunni ci sono sella tua\n");
+	scanf("%d", N); //richiedo il numero di alunni
 }
 
-int calculations(){
-    float vettore[giorni];
+int calcolo_contagiati(int *contagiati, int N, int R){
+    //calcolo il numero di giorni necessari per contagiati tutti
+    int n_giorni = 0;
+    int i = 0;
+    *(contagiati+i) = 1; //i contagiati nel giorno 0 sono 1
 
-    vettore[0]=1;
+	for (i = 0; N > contagiati[i]; i++) //il ciclo ripete finchè tutti gli alunni non sono stati contagiati
+	{
+		contagiati = realloc(contagiati, ((i+2)*sizeof(int)));//aumento la dimensione dell'array per poter inserire nuovamente il numero di contagiati
+		
+		contagiati[i + 1] = contagiati[i] + (contagiati[i] * R); //calcolo il numero di contagiati
 
-    for (int i=1; i<=giorni; ++i){
-
-        int a=vettore[i-1];
-        vettore[i]=a*R;
-        printf("Giorno: %d\tcontagiati: %.2f\n", i, vettore[i]);
-
-        if (vettore[i]>=N){
-            return i;
-        }
-    }
+		n_giorni = i + 1;
+		
+		printf("Giorno %d:\nContagiati:%d\n\n", n_giorni, contagiati[i]);
+	}
+    return ++n_giorni;
 }
-
-
-
-
